e5c18591c04458f34d3cec7828555499d07ea47b
Mehdi <elmehdiettsouli@gmail.com>
2026-09-17 14:15:28 +0200

    ajout index.html + liaison js

--- algo/algorithmes.js
+++ algo/algorithmes.js
@@ -1,16 +1,28 @@
 // 1. Fonction fizzbuzz
 function fizzbuzz(n) {
   for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
       console.log("fizz");
     } else if (i % 5 === 0) {
       console.log("buzz");
-    } else if (i % 3 === 0 && i % 5 === 0) {
-      console.log("fizzbuzz");
     } else {
       console.log(i);
     }
   }
 }
-
 fizzbuzz(15);
+
+// // 2. Fonction palindrome
+// function isPalindrome(str) {
+//   // Votre code ici
+// }
+// console.log(isPalindrome("kayak")); // true
+// console.log(isPalindrome("hello")); // false
+
+// // 3. Fonction anagramme
+// function areAnagrams(str1, str2) {
+// // Votre code ici
+// }
+// console.log(areAnagrams("listen", "silent")); // true
