5d28fb88b0b72627dff447bb2dafa57ac0ec38e9
Mehdi <elmehdiettsouli@gmail.com>
2026-09-17 16:55:51 +0200

    feat: ajout requetes sql

--- exercices/sql/bibliotheque.sql
+++ exercices/sql/bibliotheque.sql
@@ -1,7 +1,7 @@
-CREATE
-DATABASE bibliotheque;USE bibliotheque;
+CREATE DATABASE bibliotheque;
+USE bibliotheque;
 
-CREATE TABKE livre {
+CREATE TABLE livre {
   id INT PRIMARY KEY AUTO_INCREMENT, 
   titre VARCHAR(255) NOT NULL,
   auteur VARCHAR(100) NOT NULL,
@@ -15,4 +15,27 @@ INSERT INTO livres (titre, auteur, annee, genre) VALUES
 ('Naruto' 'Masashi Kishimoto', 2002, 'fantasy'),
 ('Dragonball', 'Akira Toriyama', 1989, 'fantasy'),
 ('Eragon', 'Cristopher Paolini', 2003, 'fantasy');
+
+---------------- REQUETE 1 ----------------
+-- SELECT *
+-- FROM livre;
+
+---------------- REQUETE 2 ----------------
+-- SELECT *  
+-- FROM livre
+-- WHERE annee > 1950;
+
+---------------- REQUETE 3 ----------------
+-- SELECT *
+-- FROM livre
+-- WHERE genre = 'Science-fiction';
+
+---------------- REQUETE 4 ----------------
+-- SELECT genre COUNT (*)
+-- FROM livre
+-- GROUP BY genre;
+
+---------------- REQUETE 5 ----------------
+-- SELECT MIN(annee) as livre_le_plus_ancien
+-- FROM livre;
 
